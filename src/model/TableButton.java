b5463dcd99119567812386ad74a50718c282aac9
Pomeisl Ferenc <ferencpomeisl@gmail.com>
2017-02-01 21:26:44 +0100

    TableButton

--- src/model/TableButton.java
+++ src/model/TableButton.java
@@ -1,7 +1,22 @@
 package model;
 
+import javax.swing.*;
+
 /**
  * Created by ferenc on 2017. 02. 01..
  */
-public class TableButton {
+public class TableButton extends JButton {
+    private int index;
+
+    public TableButton(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 }
