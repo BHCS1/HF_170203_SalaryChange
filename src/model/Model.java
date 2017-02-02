cf0270fd7dea91eba3b8dda5efcbaba23bb57ff0
Gabor Nagy <gabor.nagy@stylersonline.com>
2017-02-02 16:01:33 +0100

    database queries

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,15 +1,29 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public abstract class Model implements DBInterface {
     protected static Connection connection=null;
 
     public static void connect() {
-        ;
+      try {
+        Class.forName(DRIVER);
+        connection=DriverManager.getConnection(
+          URL, USERNAME, PASSWORD);
+      }
+      catch(Exception e) {
+        e.printStackTrace();
+      }
     }
 
     public static void disconnect() {
-        ;
+      try {
+        if(connection!=null)
+          connection.close();
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
     }
 }
