974f89dced8cdb2f27dd9ee6ab4de77ce9fbdc5a
krampy <krampy>
2017-02-02 13:53:15 +0100

    Jdialog kesz, mar csak a department salary sum kell és az update fv-t megbeszélni a Gáborral

--- src/model/DataSheet.java
+++ src/model/DataSheet.java
@@ -1,41 +1,114 @@
 package model;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
 public class DataSheet extends JDialog implements ActionListener {
 
   private Employee employee;
+  private JTextField tfNewSalary;
+  private JLabel lbName, lbCurrentSalary, lbNewSalary, lbMinMaxSalary;
+  private JPanel pnDetails = new JPanel();
+  private JPanel pnApprove = new JPanel();
+  private JPanel pnSalaryChange = new JPanel();
+  private JPanel pnCenter = new JPanel();
+  private JPanel pnCenterValues = new JPanel();
+  private int typedValue, salaryMin, salaryMax;
+  private boolean salaryCheck;
   
-  public DataSheet(Frame owner, Employee employee) {
-    super(owner, employee.getName(), true);
+  public DataSheet(Frame owner, Employee employee) throws ClassNotFoundException, SQLException {
+    super(owner, "Salary change", true);
     this.employee = employee;
     setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-    setSize(400, 400);
+    setSize(400, 170);
+    add(pnDetails, BorderLayout.NORTH);
+    pnDetails.setLayout(new BorderLayout());
+    lbName=new JLabel("<html><br>Employee's name: "+employee.getName()+"<html>");
+    lbCurrentSalary=new JLabel("Current salary: "+employee.getSalary()+" $");
+    pnDetails.add(lbName,BorderLayout.NORTH);
+    pnDetails.add(lbCurrentSalary,BorderLayout.SOUTH);
+    lbName.setHorizontalAlignment(SwingConstants.CENTER);
+    lbCurrentSalary.setHorizontalAlignment(SwingConstants.CENTER);
+    pnSalaryChange.setLayout(new BorderLayout());
+    add(pnSalaryChange, BorderLayout.CENTER);
+    pnCenter.setLayout(new FlowLayout());
+    pnSalaryChange.add(pnCenter, BorderLayout.NORTH);
+    lbNewSalary=new JLabel("New salary: ");
+    pnCenter.add(lbNewSalary);
+    tfNewSalary= new JTextField(8);
+    pnCenter.add(tfNewSalary);
+    salaryCalculate();
+    lbMinMaxSalary = new JLabel("Please select salary from "+salaryMin+"$ to " +salaryMax+"$");
+    pnSalaryChange.add(pnCenterValues, BorderLayout.SOUTH);
+    pnCenterValues.add(lbMinMaxSalary);
     
-    // TODO
+    JButton btSave = new JButton("Save");
+    JButton btCancel = new JButton("Cancel");
+    add(pnApprove, BorderLayout.SOUTH);
     
-    add(new JLabel("" + employee.getSalary()));
-    JButton bt = new JButton("Save");
-    add(bt, BorderLayout.PAGE_END);
-    bt.addActionListener(this);
+    pnApprove.add(btSave, BorderLayout.WEST);
+    pnApprove.add(btCancel, BorderLayout.EAST);
+    btSave.addActionListener(this);
+    btCancel.addActionListener(this);
     setLocationRelativeTo(owner);
   }
+  
+  void salaryCalculate(){
+    salaryMin=(int) Math.round(employee.getSalary()*0.95);
+    salaryMax=(int) Math.round(employee.getSalary()*1.05);
+  }
+  
+  boolean typedSalaryValueCheck(){
+    if (typedValue<salaryMin || typedValue>salaryMax)
+      return false;
+    return true;
+  }
+  
+  
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    
-    // TODO
-    
-    employee.setSalary((int) (employee.getSalary() * 1.05));
-    dispose();
+    if (e.getActionCommand().equals("Cancel"))
+      dispose();
+    else {
+      try {
+        typedValue=Integer.parseInt(tfNewSalary.getText());
+      }
+      catch (NumberFormatException ex){
+        JOptionPane.showMessageDialog(this, "Please type just a valid number!", "Information Message", JOptionPane.INFORMATION_MESSAGE);
+        return;
+      }
+        if (typedValue==employee.getSalary()) {
+          JOptionPane.showMessageDialog(this, "Same salary typed, please type again!", "Information Message", JOptionPane.INFORMATION_MESSAGE);
+          return;
+        }
+        if (!typedSalaryValueCheck()) {
+          JOptionPane.showMessageDialog(this, "Wrong salary! Please select salary from "+salaryMin+"$ to " +salaryMax+"$", "Information Message", JOptionPane.INFORMATION_MESSAGE);
+          return;
+        }
+        else {
+          if (employee.update())
+            employee.setSalary(typedValue);
+          else {
+            JOptionPane.showMessageDialog(this, "Database error, please try again...", "Information Message", JOptionPane.INFORMATION_MESSAGE);
+            return;
+          }
+        }
+        dispose();
+    }
   }
-  
-  
-  
 }
+  
+  
